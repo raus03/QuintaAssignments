36b3b86038ef38fa0835551b16cad28a7e0d0a3c
Raushan Kumar <66757269+raus03@users.noreply.github.com>
2021-08-15 14:58:11 +0530

    Add files via upload

--- EmployeesMongoDB/src/main/java/com/example/demo/EmployeeController.java
+++ EmployeesMongoDB/src/main/java/com/example/demo/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,9 +32,9 @@ public class EmployeeController {
 		return employeeService.getAllEmployee();
 	}
 	
-	@GetMapping("/getAllEmployees/{id}")
-	public Optional<Employees> getEmployee(@PathVariable String id){
-		return employeeRepo.findById(id);
+	@GetMapping("/getAllEmployeeRegion")
+	public List<Employees> getAllEmployeeRegion(@RequestParam String region){
+		return employeeService.getAllEmployeeRegion(region);
 	}
 
 }
--- EmployeesMongoDB/src/main/java/com/example/demo/EmployeeRepo.java
+++ EmployeesMongoDB/src/main/java/com/example/demo/EmployeeRepo.java
@@ -1,9 +1,15 @@
 package com.example.demo;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepo extends MongoRepository<Employees, String> {
+public interface EmployeeRepo extends MongoRepository<Employees, Integer> {
+
+	@Query(value = "{region:?0}")
+	List<Employees> findByRegion(String region);
 
 }
